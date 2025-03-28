af9d6a48db5bce99131ad1a4377e555e2ea97372
wjh <wjh@mulangcloud.com>
2025-03-28 12:06:05 +0800

    fix: code style.

--- continew-common/src/main/java/top/continew/admin/common/config/exception/GlobalExceptionHandler.java
+++ continew-common/src/main/java/top/continew/admin/common/config/exception/GlobalExceptionHandler.java
@@ -94,7 +94,8 @@ public class GlobalExceptionHandler {
         } else {
             return defaultFail;
         }
-        return R.fail(String.valueOf(HttpStatus.BAD_REQUEST.value()), "请上传小于 %s MB 的文件".formatted(Long.parseLong(sizeLimit) / 1024 / 1024));
+        return R.fail(String.valueOf(HttpStatus.BAD_REQUEST.value()), "请上传小于 %s MB 的文件".formatted(Long
+            .parseLong(sizeLimit) / 1024 / 1024));
     }
 
     /**
